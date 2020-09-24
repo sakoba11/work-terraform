e51ac960c0f970bd273784ef8dd82fa2f96ef71c
thierno <sakoba21@gmail.com>
2020-09-24 08:35:04 +0200

    add aws app from terraform

---
+++ tp-6/dev/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "PUT YOUR OWN"
+  secret_key = "PUT YOUR OWN"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-thierno"
+    key    = "thierno.tfstate"
+    region = "us-east-1"
+    access_key = "PUT YOUR OWN"
+    secret_key = "PUT YOUR OWN"
+  }
+}
+
+module "ec2" {
+  source = "../modules/ec2module"
+  instancetype = "t2.nano"
+  aws_common_tag = {
+    Name = "ec2-dev-thierno"
+  }
+  sg_name = "dev-thierno-sg"
+}
---
+++ tp-6/prod/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "PUT YOUR OWN"
+  secret_key = "PUT YOUR OWN"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-thierno"
+    key    = "thierno-prod.tfstate"
+    region = "us-east-1"
+    access_key = "PUT YOUR OWN"
+    secret_key = "PUT YOUR OWN"
+  }
+}
+
+module "ec2" {
+  source = "../modules/ec2module"
+  instancetype = "t2.micro"
+  aws_common_tag = {
+    Name = "ec2-prod-thierno"
+  }
+  sg_name = "prod-thierno-sg"
+}
